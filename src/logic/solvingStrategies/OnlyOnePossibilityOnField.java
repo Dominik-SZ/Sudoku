bf2106f183b7d28ab5017084c684c90ba876f23f
Dominik SZ <dominik.schindler-zins@gmx.de>
2017-04-29 21:42:47 +0200

    outsource the intersection solving strategies into own classes

--- src/logic/solvingStrategies/OnlyOnePossibilityOnField.java
+++ src/logic/solvingStrategies/OnlyOnePossibilityOnField.java
@@ -1,8 +1,13 @@
 package logic.solvingStrategies;
 
+import logic.Sudoku;
 import logic.SudokuSolver;
 
-
+/**
+ * Iterates the whole possibility array and searches for fields with only
+ * one possibility remaining. If such a field is found, the only remaining
+ * value is inserted via insertValue().
+ */
 public class OnlyOnePossibilityOnField implements SolvingStrategy {
 	private Sudoku sudoku;
 	private SudokuSolver solver;
@@ -21,7 +26,7 @@ public class OnlyOnePossibilityOnField implements SolvingStrategy {
 			for (int j = 0; j < length; j++) {
 				if (sudoku.getPossibilities(i, j).size() == 1) {
 					int onlyPossibility = (int) sudoku.getPossibilities(i, j).toArray()[0];
-					insertValue(onlyPossibility, i, j, true);
+					solver.insertValue(onlyPossibility, i, j, true);
 					answer = true;
 				}
 			}
