17bb229a9cdce7bc38b070b76e2f1e22a8093532
Nico Weidmann <nicoweidmann@web.de>
2017-04-29 23:45:40 +0200

    Refactor OnlyOnePossibilityOnField
     - removed unnecessary attribute length
     - removed unnecessary braces
     - indented with spaces instead of tabs

--- src/logic/solvingStrategies/OnlyOnePossibilityOnField.java
+++ src/logic/solvingStrategies/OnlyOnePossibilityOnField.java
@@ -1,27 +1,26 @@
 package logic.solvingStrategies;
 
 import logic.SudokuSolver;
+import logic.Sudoku;
 
 
 public class OnlyOnePossibilityOnField implements SolvingStrategy {
 	private Sudoku sudoku;
 	private SudokuSolver solver;
-	private int length;
 
 	OnlyOnePossibilityOnField (Sudoku sudoku, SudokuSolver solver) {
 		this.sudoku = sudoku;
 		this.solver = solver;
-		this.length = sudoku.getLength();
 	}
 
 	@Override
 	public boolean apply() {
 		boolean answer = false;
-		for (int i = 0; i < length; i++) {
-			for (int j = 0; j < length; j++) {
+		for (int i = 0; i < sudoku.getLength(); i++) {
+			for (int j = 0; j < sudoku.getLength(); j++) {
 				if (sudoku.getPossibilities(i, j).size() == 1) {
 					int onlyPossibility = (int) sudoku.getPossibilities(i, j).toArray()[0];
-					insertValue(onlyPossibility, i, j, true);
+					solver.insertValue(onlyPossibility, i, j, true);
 					answer = true;
 				}
 			}
@@ -31,7 +30,7 @@ public class OnlyOnePossibilityOnField implements SolvingStrategy {
 
 	@Override
 	public int getDifficulty() {
-		return(1);
+		return 1;
 	}
 
 }
